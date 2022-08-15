43c02efe7aedbe303d8ab59d2171e23bc574bc40
Michaela Healton <mshealton@gmail.com>
2022-08-15 16:02:59 -0700

    Read in, and alphabetize ratings, and print them

--- ratings.py
+++ ratings.py
@@ -1,4 +1,15 @@
 """Restaurant rating lister."""
 
+def read_ratings(filename):
+    ratings = open(filename)
+    ratings_dict = {}
+    for line in ratings:
+        restaurant, rating = line.split(":")
+        ratings_dict[restaurant] = rating 
 
-# put your code here
+    alphabetical_list = sorted(ratings_dict.items())
+    
+    for item in alphabetical_list:
+        print(item[0] + " is rated at " + item[1])
+
+read_ratings('scores.txt')
