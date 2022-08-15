1e6b5c454f2f2376a842141c7e95a345f5bc087d
Michaela Healton <mshealton@gmail.com>
2022-08-15 16:48:53 -0700

    Give user options

--- ratings.py
+++ ratings.py
@@ -14,23 +14,34 @@ def read_ratings(filename):
         restaurant, rating = line.split(":")
         ratings_dict[restaurant] = rating 
 
-    user_restaurant = input("Please enter a restaurant: ")
-    user_rating = input("Please give your rating: ")
+    user_choice = 0
+    while user_choice != "q":
+        print("""
+        If you want to:
+            see a list of all the restaurants and ratings ENTER 1
+            add a rating of your own ENTER 2
+            quit ENTER q
+        """)
+        user_choice = input("-> ")
+        if user_choice == "2":
+            user_restaurant = input("Please enter a restaurant: ")
+            user_rating = input("Please give your rating: ")
 
-    while True: 
-        try:
-            if int(user_rating) > 5 or int(user_rating) < 1:
-                user_rating = input("Please give a rating between 1 and 5: ")
-            else:
-                break
-        except ValueError:
-            user_rating = input("Please enter a number between 1 and 5: ")
+            while True: 
+                try:
+                    if int(user_rating) > 5 or int(user_rating) < 1:
+                        user_rating = input("Please give a rating between 1 and 5: ")
+                    else:
+                        break
+                except ValueError:
+                    user_rating = input("Please enter a number between 1 and 5: ")
 
-    ratings_dict[user_restaurant] = user_rating
-
-    alphabetical_list = sorted(ratings_dict.items())
-    
-    for item in alphabetical_list:
-        print(item[0] + " is rated at " + item[1])
+            ratings_dict[user_restaurant] = user_rating
+        elif user_choice == "1":
+            alphabetical_list = sorted(ratings_dict.items())
+            
+            for item in alphabetical_list:
+                print(item[0] + " is rated at " + item[1])
+        
 
 read_ratings('scores.txt')
