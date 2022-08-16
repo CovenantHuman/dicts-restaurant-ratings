27bf00913c02260dc9375203f975565ee14fb830
Michaela Healton <mshealton@gmail.com>
2022-08-15 17:16:12 -0700

    Give user option to rate a random restaurant

--- ratings.py
+++ ratings.py
@@ -1,4 +1,5 @@
 """Restaurant rating lister."""
+import random
 
 def read_ratings(filename):
     """Alphabetize and print restaurant ratings
@@ -20,6 +21,8 @@ def read_ratings(filename):
         If you want to:
             see a list of all the restaurants and ratings ENTER 1
             add a rating of your own ENTER 2
+            change the rating of a random restaurant ENTER 3
+            change the rating of a specific restaurant ENTER 4
             quit ENTER q
         """)
         user_choice = input("-> ")
@@ -42,6 +45,24 @@ def read_ratings(filename):
             
             for item in alphabetical_list:
                 print(item[0] + " is rated at " + item[1])
+        elif user_choice == "3":
+            restaurant_list = list(ratings_dict.keys())
+            random_restaurant = random.choice(restaurant_list)
+            print("The restaurant is", random_restaurant)
+            print(random_restaurant + "'s current rating is " 
+            + ratings_dict[random_restaurant])
+            user_rating = input("What would you like to rate it: ")
+            while True: 
+                try:
+                    if int(user_rating) > 5 or int(user_rating) < 1:
+                        user_rating = input("Please give a rating between 1 and 5: ")
+                    else:
+                        break
+                except ValueError:
+                    user_rating = input("Please enter a number between 1 and 5: ")
+            ratings_dict[random_restaurant] = user_rating
+        elif user_choice == "4":
+            pass
         
 
 read_ratings('scores.txt')
